d780db49023e44d2d31233f91292ecddfc2b78bd
hanxiaolc <hanxiaolc@gmail.com>
2018-06-13 17:40:01 +0900

    initial

---
+++ src/main/java/jp/co/basenet/accopening/AccopeningApplication.java
@@ -0,0 +1,12 @@
+package jp.co.basenet.accopening;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccopeningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccopeningApplication.class, args);
+	}
+}
